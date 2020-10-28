9433fa5975f69f6c9653a767b7666fd4207f7f04
Nathnael Getahun <contact.nathnael@gmail.com>
2020-10-28 20:57:16 +0300

    Fixed config issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "mail.google.com",    
-    :user_name: ENV["MAILER_EMAIL"],
-    :password: ENV["MAILER_PASSWORD"]
+    :user_name => ENV["MAILER_EMAIL"],
+    :password => ENV["MAILER_PASSWORD"],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
